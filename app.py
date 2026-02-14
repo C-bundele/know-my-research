9bfffb725d5d821cdf9345d94a94b610e39b1827
C-bundele <chanchalbundele04@gmail.com>
2026-02-13 21:11:01 -0500

    Src completed setup, and personal document setup cleaned

--- app.py
+++ app.py
@@ -8,35 +8,39 @@ from src.search import RAGSearch
 # Example usage
 if __name__ == "__main__":
     
-    docs = load_all_documents("data")
-    store = FaissVectorStore("faiss_store")
-    store.build_from_documents(docs)
-    store.load()
-    print(store.query("What is clustering and KNN?", top_k=3))
+    # docs = load_all_documents("data")
+    # store = FaissVectorStore("faiss_store")
+    # store.build_from_documents(docs)
+    # store.load()
+    # print(store.query("What is Image captioning?", top_k=3))
     rag_search = RAGSearch()
-    query = "What is clustering and KNN?"
+    query = input("Enter your query: ")
     summary = rag_search.search_and_summarize(query, top_k=3)
     print("Summary:", summary)
-    print(docs)
-# if __name__ == "__main__":
-    
-#     docs = load_all_documents("data")
-#     emb_pipe = EmbeddingPipeline()
-#     chunks = emb_pipe.chunk_documents(docs)
-#     embeddings = emb_pipe.embed_chunks(chunks)
-#     print("[INFO] Example embedding:", embeddings[0] if len(embeddings) > 0 else None)
+    #print(docs)
 
-# if __name__ == "__main__":
-    
-#     docs = load_all_documents("data")
-#     store = FaissVectorStore("faiss_store")
-#     store.build_from_documents(docs)
-#     store.load()
-#     print(store.query("What is clustering?", top_k=3))
 
-# Example usage
+# # if __name__ == "__main__":
+    
+# #     docs = load_all_documents("data")
+# #     emb_pipe = EmbeddingPipeline()
+# #     chunks = emb_pipe.chunk_documents(docs)
+# #     embeddings = emb_pipe.embed_chunks(chunks)
+# #     print("[INFO] Example embedding:", embeddings[0] if len(embeddings) > 0 else None)
+
+# # if __name__ == "__main__":
+    
+# #     docs = load_all_documents("data")
+# #     store = FaissVectorStore("faiss_store")
+# #     store.build_from_documents(docs)
+# #     store.load()
+# #     print(store.query("What is image captioning?", top_k=3))
+
+# # Example usage
 # if __name__ == "__main__":
 #     rag_search = RAGSearch()
-#     query = "What is clustering and  KNN?"
+#     query = "What is image captioning?"
 #     summary = rag_search.search_and_summarize(query, top_k=3)
 #     print("Summary:", summary)
+
+
